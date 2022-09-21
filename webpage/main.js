2b206ed3961ffea77e6321ee37f43651d7c04cfd
Pontus Hedman <pontalajnen@pop-os.localdomain>
2022-09-21 22:24:09 +0200

    Done

--- webpage/main.js
+++ webpage/main.js
@@ -12,16 +12,27 @@ const n = 100;
 const cars = generateCars(n);
 let bestCar = cars[0];
 if (localStorage.getItem("bestBrain")) {
-    bestCar.brain = JSON.parse(
-        localStorage.getItem("bestBrain")
-    );
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].brain = JSON.parse(
+            localStorage.getItem("bestBrain")
+        );
+        if (i != 0) {
+            NeuralNetwork.mutate(cars[i].brain, 0.2);
+        }
+    }
+    
 }
 
 
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
     new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
-    new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2) 
+    new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2), 
+    new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2), 
+    new Car(road.getLaneCenter(2), -500, 30, 50, "DUMMY", 2), 
+    new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2), 
+    new Car(road.getLaneCenter(0), -700, 30, 50, "DUMMY", 2), 
+    new Car(road.getLaneCenter(1), -900, 30, 50, "DUMMY", 2) 
 ];
 
 animate();
